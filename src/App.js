4ab860293310846fab6d2084476a576055163122
arunprasad-07 <arunprasad.azhagesan@gmail.com>
2021-05-13 22:27:13 +0530

    Initial meetup list from static data completed.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupPage from './pages/AllMeetups';
+import Favorites from './pages/Favorites';
+import NewMeetup from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
