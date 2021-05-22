0f89cb9faacb370a58b0fd20a5e0242f0d646245
arunprasad-07 <arunprasad.azhagesan@gmail.com>
2021-05-22 23:49:06 +0530

    add new meetup to firebase db.

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,28 +1,51 @@
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
+import { useRef } from 'react';
 
 function NewMeetupForm(props) {
+  const refTitle = useRef();
+  const refAddress = useRef();
+  const refURL = useRef();
+  const refDescription = useRef();
+
   function submitHandler(event) {
     event.preventDefault();
+    const enteredTitle = refTitle.current.value;
+    const enteredAddress = refAddress.current.value;
+    const enteredURL = refURL.current.value;
+    const enteredDescription = refDescription.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      address: enteredAddress,
+      url: enteredURL,
+      description: enteredDescription,
+    };
+    props.onAddNewMeetup(meetupData);
   }
   return (
     <Card>
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" id="title" required />
+          <input type="text" id="title" ref={refTitle} required />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" id="image" required />
+          <input type="url" id="image" ref={refURL} required />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Meetup Address</label>
-          <input type="text" id="address" required />
+          <input type="text" id="address" ref={refAddress} required />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Meetup Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea
+            id="description"
+            ref={refDescription}
+            required
+            rows="5"
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,27 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import { useHistory } from 'react-router-dom';
+
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function addNewMeetupHandler(params) {
+    fetch(
+      'https://meetup-api-63fe5-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(params),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
   return (
     <section>
       <h2>Add New Meetup</h2>
-      <NewMeetupForm />
+      <NewMeetupForm onAddNewMeetup={addNewMeetupHandler} />
     </section>
   );
 }
